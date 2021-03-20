d84dccd4d421b79fa3fa12162f64e3560a43611a
vishalrajapure <vishalrajapure@gmail.com>
2021-03-20 14:12:35 +0530

    Checking automatic deploys

--- src/main/java/com/helping/controller/RegistrationController.java
+++ src/main/java/com/helping/controller/RegistrationController.java
@@ -9,22 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/helping")
 public class RegistrationController {
 
-   /* @Autowired
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    private EmployeeRegistrationService employeeRegistrationService;
-
-    @PostMapping("/registerEmployee")
-    public Employee registerPatient(@RequestBody @Valid Employee employee) {
-        return employeeRegistrationService.registerEmployee(employee);
-    }
-
-    @PostMapping("/registerAdmin")
-    public Admin registerAdmin(@RequestBody @Valid Admin admin) {
-        return employeeRegistrationService.registerAdmin(admin);
-    }*/
-
     @GetMapping("/testHealth")
     public String testHealth(){
         return "Helping hand is accessible!!";
